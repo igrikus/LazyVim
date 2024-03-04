1e1d2e4bc64e0ff0ffb7d7c80105c7b0deed1603
Igor Kachurin <ikachurin@ds-in.net>
2024-03-04 16:36:30 +0100

    Initial commit

---
+++ lua/plugins/scratch.lua
@@ -0,0 +1,8 @@
+return {
+  "LintaoAmons/scratch.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<M-C-n>", "<cmd>Scratch<cr>", desc = "Create scratch file" },
+    { "<M-C-o>", "<cmd>ScratchOpen<cr>", desc = "Open scratch file" },
+  },
+}
