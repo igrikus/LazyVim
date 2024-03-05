6b616a0c49eb0f6bcd5d7decce61fd0b3a5a59a5
Igor Kachurin <ikachurin@ds-in.net>
2024-03-05 16:14:10 +0100

    enable dap code extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.dap.core" },
 
     -- import/override with your plugins
     { import = "plugins" },
