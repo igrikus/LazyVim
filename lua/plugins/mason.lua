e71b572a8e59a5f94580efd31f3a7d986841d185
Igor Kachurin <ikachurin@ds-in.net>
2024-03-14 16:20:48 +0100

    remove ansible from mason ensure_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,10 +2,6 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
-      -- Ansible
-      "ansible-language-server",
-      "ansible-lint",
-
       -- Bash
       "bash-language-server",
       "shellcheck",
