65b52f1f7363fd71ceaa97afcd1cdd6487813cb5
igrikus <igor.kachurin.2001@gmail.com>
2024-04-08 11:21:15 +0200

    enable lang typescript extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.docker" },
