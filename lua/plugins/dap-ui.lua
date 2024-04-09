dda897e9e71c1e6d7311c8b6c4d45b90201c9126
igrikus <igor.kachurin.2001@gmail.com>
2024-04-09 14:39:34 +0200

    do not close dap-ui after session ends

---
+++ lua/plugins/dap-ui.lua
@@ -0,0 +1,12 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  -- stylua: ignore
+  config = function(_, opts)
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup(opts)
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open({})
+    end
+  end,
+}
