e35920c0acf26383af87c2132124183c5bb56a9b
igrikus <igor.kachurin.2001@gmail.com>
2024-05-23 17:29:52 +0200

    neo-tree change cwd of neovim

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,35 +1,38 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      bind_to_cwd = true,
+    },
     event_handlers = {
       {
         event = "neo_tree_buffer_enter",
         handler = function()
           vim.opt.relativenumber = true
         end,
-      }
+      },
     },
     window = {
       mappings = {
         ["h"] = function(state)
           local node = state.tree:get_node()
-          if node.type == 'directory' and node:is_expanded() then
-            require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+          if node.type == "directory" and node:is_expanded() then
+            require("neo-tree.sources.filesystem").toggle_directory(state, node)
           else
-            require 'neo-tree.ui.renderer'.focus_node(state, node:get_parent_id())
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
           end
         end,
         ["l"] = function(state)
           local node = state.tree:get_node()
-          if node.type == 'directory' then
+          if node.type == "directory" then
             if not node:is_expanded() then
-              require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+              require("neo-tree.sources.filesystem").toggle_directory(state, node)
             elseif node:has_children() then
-              require 'neo-tree.ui.renderer'.focus_node(state, node:get_child_ids()[1])
+              require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
             end
           end
         end,
-      }
-    }
-  }
+      },
+    },
+  },
 }
