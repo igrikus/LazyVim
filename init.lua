11b295adf1fa50776f3e6ccfc12d369c21331d73
igrikus <igor.kachurin.2001@gmail.com>
2024-06-10 14:47:51 +0200

    remove venv auto-activation for new plugin version

--- init.lua
+++ init.lua
@@ -11,15 +11,3 @@ vim.api.nvim_create_autocmd("UiEnter", {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*",
-  callback = function()
-    local venv = vim.fn.finddir("venv", vim.fn.getcwd())
-    if venv ~= "" then
-      require("venv-selector").retrieve_from_cache()
-    end
-  end,
-  once = true,
-})
