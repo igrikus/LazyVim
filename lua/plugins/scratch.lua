ac9d0a540d458815fe8ceb6fd1ad9421d0f60cca
igrikus <igor.kachurin.2001@gmail.com>
2024-06-10 14:44:11 +0200

    update scratch.nvim

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -1,9 +1,22 @@
 return {
   "LintaoAmons/scratch.nvim",
-  version = "0.13.2",
   event = "VeryLazy",
   keys = {
     { "<M-C-n>", "<cmd>Scratch<cr>", desc = "Create scratch file" },
     { "<M-C-o>", "<cmd>ScratchOpen<cr>", desc = "Open scratch file" },
+  },
+  opts = {
+    filetypes = {
+      "xml",
+      "go",
+      "lua",
+      "js",
+      "py",
+      "sh",
+      "ps1",
+      "json",
+      "yaml",
+      "txt",
+    },
   },
 }
