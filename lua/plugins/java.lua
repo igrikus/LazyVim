e3443451d6c3c2d0207e0ee5a7b1241c19e55cd8
igrikus <igor.kachurin.2001@gmail.com>
2024-08-09 15:21:13 +0200

    disabled jdtls formatting

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -4,6 +4,13 @@ return {
     opts = {
       jdtls = function(opts)
         opts.root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml" })
+        opts.settings = {
+          java = {
+            format = {
+              enabled = false,
+            },
+          },
+        }
         return opts
       end,
     },
