468f98eb176585763ad239d4469ab7df5f274843
igrikus <igor.kachurin.2001@gmail.com>
2024-08-09 16:49:02 +0200

    indentation fix for java scratch file creation

--- lua/plugins/scratch.lua
+++ lua/plugins/scratch.lua
@@ -23,9 +23,9 @@ return {
       java = {
         content = {
           "class Scratch {",
-          "   public static void main(String []args) {",
-          "       ",
-          "   }",
+          "    public static void main(String []args) {",
+          "        ",
+          "    }",
           "}",
         },
         cursor = {
