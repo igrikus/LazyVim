be1ca0ec530c8bfceaab98093d04b847737dda04
igrikus <igor.kachurin.2001@gmail.com>
2024-11-12 15:35:41 +0100

    no need for clear terminal shortcut anymore

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("t", "<C-l>") -- Clear terminal with Ctrl+l
 vim.keymap.del("n", "<C-_>") -- Disable built-in terminal
 
 vim.keymap.set({ "n", "v" }, "x", '"_x') -- Don't yank when cutting
