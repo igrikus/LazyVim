e5ccf3c827f0bb693e8b4734254ad3a0fb97ddb8
igrikus <igor.kachurin.2001@gmail.com>
2024-12-13 12:49:03 +0100

    overwrite grep and find shortcuts
    use cwd instead of root

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+  },
+}
