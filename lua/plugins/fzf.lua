f2f7049796c7d0de7c7db33bc740d9aad28fa8d1
igrikus <igor.kachurin.2001@gmail.com>
2024-12-14 13:48:13 +0100

    bump plugin versions

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,5 +1,5 @@
 return {
-  "nvim-telescope/telescope.nvim",
+  "ibhagwan/fzf-lua",
   keys = {
     { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
     { "<leader>/", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
