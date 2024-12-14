41a720c45fa0240ff44c30ffff5719a572c2bef6
igrikus <igor.kachurin.2001@gmail.com>
2024-12-14 14:05:47 +0100

    neo-tree swap cwd and root dir keybindings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+  },
   opts = {
     filesystem = {
       bind_to_cwd = true,
