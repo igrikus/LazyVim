14771d2a5daf314a07f01fb5c60821f42419ac82
igrikus <igor.kachurin.2001@gmail.com>
2025-01-02 13:07:25 +0100

    disable venv autocmd while plugin is disabled
    https://github.com/LazyVim/LazyVim/pull/5248

--- init.lua
+++ init.lua
@@ -1,18 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  desc = "Auto select virtualenv on Nvim open",
-  pattern = "*",
-  callback = function()
-    local venv_paths = { "venv", ".venv", "env", ".env" }
-    for _, path in ipairs(venv_paths) do
-      local venv = vim.fn.finddir(path, vim.fn.getcwd())
-      if venv ~= "" then
-        require("venv-selector").retrieve_from_cache()
-        break
-      end
-    end
-  end,
-  once = true,
-})
