19119baebe48948217e288e55eecf0eff2d526c6
igrikus <igor.kachurin.2001@gmail.com>
2025-07-11 12:47:01 +0200

    use gemini-cli in separate terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,4 +5,22 @@ return {
     open_mapping = [[<c-\>]],
     direction = "float",
   },
+  config = function(_, opts)
+    require("toggleterm").setup(opts)
+
+    local Terminal = require("toggleterm.terminal").Terminal
+    local gemini = Terminal:new({
+      cmd = "gemini",
+      dir = vim.fn.getcwd(),
+      direction = "float",
+      hidden = true,
+      count = 9,
+    })
+
+    function _G.toggle_gemini()
+      gemini:toggle()
+    end
+
+    vim.keymap.set("n", "<leader>aa", "<cmd>lua _G.toggle_gemini()<CR>", { desc = "Toggle gemini terminal" })
+  end,
 }
