a7a6e73a5e102b615141b6bda74ca0d492b96140
igrikus <igor.kachurin.2001@gmail.com>
2025-07-15 15:26:29 +0200

    close gemini terminal using the same keymap like others

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,14 +7,18 @@ return {
   },
   config = function(_, opts)
     require("toggleterm").setup(opts)
-
     local Terminal = require("toggleterm.terminal").Terminal
+
     local gemini = Terminal:new({
       cmd = "gemini",
       dir = vim.fn.getcwd(),
       direction = "float",
       hidden = true,
       count = 9,
+      on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
     })
 
     function _G.toggle_gemini()
