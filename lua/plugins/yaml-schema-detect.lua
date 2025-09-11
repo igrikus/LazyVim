15968f27a52b77b68ac331562fd4b54da38fa16e
igrikus <igor.kachurin.2001@gmail.com>
2025-09-11 11:09:30 +0200

    auto-detect yaml schema

---
+++ lua/plugins/yaml-schema-detect.lua
@@ -0,0 +1,10 @@
+return {
+  "cwrau/yaml-schema-detect.nvim",
+  ---@module "yaml-schema-detect"
+  ---@type YamlSchemaDetectOptions
+  opts = {}, -- use default options
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  ft = { "yaml" },
+}
