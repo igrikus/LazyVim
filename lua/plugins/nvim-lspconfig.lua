7df5b433f2c9227676976d7947557d04acb97723
igrikus <igor.kachurin.2001@gmail.com>
2026-06-26 11:28:26 +0200

    fix(lspconfig): session hand on some terraform files

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,15 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      terraformls = {
+        -- Disable LSP semantic tokens for Terraform.
+        -- terraform-ls can emit a semantic token whose length runs past the end
+        -- of the buffer. Neovim has a problem with it, so it's better to disable it.
+        -- Highlighting still comes from treesitter.
+        on_attach = function(client)
+          client.server_capabilities.semanticTokensProvider = nil
+        end,
+      },
       pyright = {
         settings = {
           python = {
